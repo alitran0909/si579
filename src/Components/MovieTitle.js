c5e2cf8361d7bea79012075d1fee748c3fc993f0
alitran0909 <57301650+alitran0909@users.noreply.github.com>
2024-12-03 04:58:23 +0000

    Added gif files

--- src/Components/MovieTitle.js
+++ src/Components/MovieTitle.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import headerGif from '/workspaces/si579/src/ghibli.gif'; // Import your GIF
+import sGif from '/workspaces/si579/src/spirtedawaywalk.gif'
+import princess from '/workspaces/si579/src/princesswalking.gif'
+import howl from '/workspaces/si579/src/howlswalking.gif'
+import ponyo from '/workspaces/si579/src/ponyorunning.gif'
 
-const MovieTitle = () => {
-  return <h1 style={{ marginLeft: '20px' }}>Studio Ghibli Films</h1>;
-};
 
-export default MovieTitle;
+function App() {
+  return (
+    <div>
+     <header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '20px' }}>
+        <h1 style={{ margin: 0 }}>Studio Ghibli Films</h1>
+        <img src={headerGif} alt="Header Animation" style={{ width: '150px', height: 'auto', margin: '10px' }} />
+        <img src={sGif} alt="Spirited Away Walk" className="gif" style={{ width: '150px', height: 'auto', margin: '10px' }} />
+        <img src={princess} alt="Princess Walking" className="gif" style={{ width: '150px', height: 'auto', margin: '10px' }} />
+        <img src={howl} alt="Howl's Walking" className="gif" style={{ width: '150px', height: 'auto', margin: '10px' }} />
+        <img src={ponyo} alt="Ponyo Running" className="gif" style={{ width: '150px', height: 'auto', margin: '10px' }} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
