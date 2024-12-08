be7498160eca93f53d7ab06b50d88932ff62e0ac
alitran0909 <57301650+alitran0909@users.noreply.github.com>
2024-12-08 04:54:19 +0000

    added env

--- src/Components/FetchApi.js
+++ src/Components/FetchApi.js
@@ -1,5 +1,6 @@
 // API FETCH
 export const generateApiUrl = (title) => {
+    apikey = process.env.REACT_APP_API_KEY
     const encodedTitle = encodeURIComponent(title).replace(/%20/g, '+');
-    return `https://www.omdbapi.com/?t=${encodedTitle}&apikey=e304f6a2`;
+    return `https://www.omdbapi.com/?t=${encodedTitle}&${apikey}`;
 };
